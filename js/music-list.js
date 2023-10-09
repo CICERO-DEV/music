77bdfca82375900c8a1e86b1c877de7373384b7e
Cicero França <146494128+CICERO-DEV@users.noreply.github.com>
2023-10-09 16:01:50 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: "is this really Happening",
+        artist: "TrackTribe",
+        img: "assets/media/img-1.jpg",
+        src: "assets/songs/music-1.mp3",
+    },
+    {
+        name: "Am I Dreaming.",
+        artist: "Spider Man. ",
+        img: "assets/media/img-2.jpg",
+        src: "assets/songs/music-2.mp3",
+    },
+    {
+        name: "Timpani Beat",
+        artist: "Nana Kuabena",
+        img: "assets/media/img-3.jpg",
+        src: "assets/songs/music-3.mp3",
+    },
+]
